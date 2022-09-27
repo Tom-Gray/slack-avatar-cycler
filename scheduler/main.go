1a71402b247023a451f16b5ca54562ec54cf2c86
Tom <swinkistom@gmail.com>
2022-09-27 15:46:22 +1000

    added scheduler logic

--- scheduler/main.go
+++ scheduler/main.go
@@ -1,9 +1,10 @@
-package main
+package scheduler
 
 import (
 	"fmt"
-	"github.com/spf13/viper"
 	"time"
+
+	"github.com/spf13/viper"
 )
 
 type Config struct {
@@ -12,7 +13,7 @@ type Config struct {
 
 type SchedulePeriods struct {
 	Image string `mapstructure:"image"`
-	Time  string `mapstructure:"time"`
+	Time  string `mapstructure:"endTime"`
 }
 
 func main() {
@@ -28,16 +29,39 @@ func main() {
 		return
 	}
 	fmt.Println(config)
+	currentSchedule := EvaluateSchedule((config))
+	fmt.Println(currentSchedule)
 
 }
 
 func checkTime(schedule string) bool {
-	now := time.Now()
-
-	if now.Format(time.Kitchen) < schedule {
-		fmt.Printf("it is past %v\n", schedule)
-		return false
+	theTime := time.Now().Format(time.Kitchen)
+	now, _ := time.Parse(time.Kitchen, theTime)
+	timeToBeEvaluated, err := time.Parse(time.Kitchen, schedule)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("It is currently %v\n", now.Format(time.Kitchen))
+	fmt.Printf("is %v before %v?\n", now.Format(time.Kitchen), timeToBeEvaluated.Format(time.Kitchen))
+	if now.Before(timeToBeEvaluated) {
+		fmt.Printf("it is not past  %v\n", schedule)
+		return true
 	}
 
-	return true
+	return false
+}
+
+// returns the current schedule.
+func EvaluateSchedule(config Config) string {
+
+	for schedules := range config.Schedule {
+		for k, v := range config.Schedule[schedules] {
+			//fmt.Printf("key: %v Value: %v", k, v.Time)
+			if checkTime(v.Time) {
+				fmt.Printf("It is not yet past %v\n", v.Time)
+				return k
+			}
+		}
+	}
+	return ""
 }
